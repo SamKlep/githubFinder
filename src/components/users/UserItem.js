3830f41bd422c6ec1c7d25d38697bf5c1a108a32
SamKlep <shamdemann23@gmail.com>
2020-09-16 11:02:33 -0500

    User data now loading from github api.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_ur } = this.props.user;
+const UserItem = ({ user: { login, avatar_url, html_ur } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        style={{ width: "60px" }}
+        alt=""
+      />
 
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          className="round-img"
-          style={{ width: "60px" }}
-          alt=""
-        />
+      <h3>{login}</h3>
 
-        <h3>{login}</h3>
+      <a href={html_ur} className="btn btn-dark btn-am my-1">
+        More
+      </a>
+    </div>
+  );
+};
 
-        <a href={html_ur} className="btn btn-dark btn-am my-1">
-          More
-        </a>
-      </div>
-    );
-  }
-}
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
